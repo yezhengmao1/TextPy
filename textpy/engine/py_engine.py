455bcc707ed28cff36b89218f678d2ee74b81da5
yezhengmao <yezhengmaolove@gmail.com>
2025-04-28 19:20:17 +0800

    [wip]

--- textpy/engine/py_engine.py
+++ textpy/engine/py_engine.py
@@ -1,4 +1,8 @@
+import logging
+
 from .engine import BaseEngine
+
+logger = logging.getLogger("PyEngine")
 
 
 class PyEngine(BaseEngine):
@@ -6,6 +10,23 @@ class PyEngine(BaseEngine):
     def __init__(self, **kwargs):
         super().__init__()
         del kwargs
+
+    def install_package(self, error_str: str):
+        import re
+        import subprocess
+
+        match = re.search(r"No module named '(.*)'\s*$", error_str)
+
+        if not match:
+            logger.warning(f"No module name in error str : {error_str}")
+            return
+
+        missing_module = match.group(1)
+
+        result = subprocess.run(["pip", "install", missing_module])
+        logger.info(f"install module result : {result}")
+
+        return
 
     def run(self, code: str, **kwargs):
         """
@@ -24,13 +45,23 @@ class PyEngine(BaseEngine):
 
         _, func = namespace.popitem()
 
-        try:
-            result = func(**kwargs)
-        except TypeError:
-            # got an unexpected keyword argument
-            raise TypeError
-        except Exception as e:
-            print(f"py engine run error: {str(e)}")
-            raise
+        try_execute_cnt = 0
+
+        while True:
+            try:
+                result = func(**kwargs)
+                break
+            except ModuleNotFoundError as e:
+                # install the module
+                self.install_package(error_str=str(e))
+                try_execute_cnt += 1
+            except TypeError:
+                # got an unexpected keyword argument
+                raise TypeError
+            except Exception as e:
+                raise
+
+            if try_execute_cnt >= 5:
+                raise RuntimeError
 
         return result
